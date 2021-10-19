2c015f86b5e4a64fea1c04c96052437db73f5472
dahui58 <dahui58@hotmail.com>
2021-10-19 17:45:34 +0100

    added right side search and additional tests

--- karate-chop/chop.go
+++ karate-chop/chop.go
@@ -8,13 +8,15 @@ func Chop(searchItem int, sortedIntSlice []int) int {
 	if len(sortedIntSlice) == 0 {
 		return -1
 	}
-	fmt.Printf("sorted int slice = %#v\n", sortedIntSlice)
 	middleIndex := len(sortedIntSlice)/2
 	middleValue := sortedIntSlice[middleIndex]
+	fmt.Printf("sorted int slice = %#v\n middle index = %d\n mid val = %d\n", sortedIntSlice, middleIndex, middleValue)
 	if searchItem == middleValue {
 		return middleIndex
 	} else if searchItem < middleValue {
 		return Chop(searchItem, sortedIntSlice[0:middleIndex])
+	} else if searchItem > middleValue {
+		return middleIndex + Chop(searchItem, sortedIntSlice[middleIndex: len(sortedIntSlice)])
 	}
 	return middleIndex
 }

3621bde8131d9b2ffc95014793138bcd3d9ad679
JP Anderson <dahui58@hotmail.com>
2023-02-17 21:17:48 +0700

    implement diff of words function to compare words

--- word-chains/wordchain.go
+++ word-chains/wordchain.go
@@ -6,3 +6,16 @@ func Solve(start, end string) []string {
 	}
 	return nil
 }
+
+func diffOfWords(s1, s2 string) int {
+	if len(s1) != len(s2) {
+		return -1
+	}
+	diff := 0
+	for i := 0; i < len(s1); i++ {
+		if s1[i] != s2[i] {
+			diff++
+		}
+	}
+	return diff
+}
--- word-chains/wordchain_test.go
+++ word-chains/wordchain_test.go
@@ -11,3 +11,28 @@ func TestReturnsEmptyListForInvalidStartEnd(t *testing.T) {
 	assert.Empty(t, result)
 }
 
+func TestDiffOfWords(t *testing.T) {
+	type testCase struct {
+		s1, s2 string
+		expectedOut int
+	}
+	
+	cases := []testCase{
+		{
+			s1: "dog", s2: "cat",
+			expectedOut: 3,
+		},
+		{
+			s1: "dog", s2: "dog",
+			expectedOut: 0,
+		},
+		{
+			s1: "dog", s2: "dig",
+			expectedOut: 1,
+		},
+	}
+
+	for _, tc := range cases {
+		assert.Equal(t, tc.expectedOut, diffOfWords(tc.s1, tc.s2))
+	}
+}
